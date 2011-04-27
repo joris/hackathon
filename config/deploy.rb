f324a2658285fcd699c270e7cb7990919c64294a
Joris Machielse <joris@duidelijk.net>
2011-04-27 11:13:10 +0200

    Configured capistrano deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,14 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "TNW Hackathon"
+set :repository,  ".git"
 
-set :scm, :subversion
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "test1", "test2", "live1", "live2"
+
+set :use_sudo, false
+
+set :deploy_to, "/var/www/"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
