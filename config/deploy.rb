30be0720c8d8a528835b6ff74ca621aa8cd6c777
Joris Machielse <joris@duidelijk.net>
2011-04-27 11:39:42 +0200

    Cap deployment now accepts servers= parameter that should contain a comma-separated list of server hostnames.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,22 +2,40 @@ set :application, "TNW Hackathon"
 set :repository,  ".git"
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "test1", "test2", "live1", "live2"
+set :branch, "master"
 
 set :use_sudo, false
+set :deploy_via, :copy
+set :copy_cache, false
+
+set :copy_exclude, [".git/*", "log/*.log"]
+set :copy_compression, :gzip
+
+
+for argument in ARGV
+  if argument =~ /^servers=/
+    servers = argument.gsub(/^servers=/, '').scan(/([^,]+),?/).flatten
+  end
+end
+
+if servers
+  for host in servers
+    server(host, :web)
+  end
+else
+  role :web, "test1", "test2", "live1", "live2"
+end
 
 set :deploy_to, "/var/www/"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart do ; end
+  # task :restart, :roles => :app, :except => { :no_release => true } do
+  #   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  # end
+end
